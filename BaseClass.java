1f6c441491338a57180fd910a6eb738464d7ac16
rinki-yadav <86030554+rinki-yadav@users.noreply.github.com>
2021-10-26 13:23:54 -0400

    Add files via upload

--- BaseClass.java
+++ BaseClass.java
@@ -1,10 +1,12 @@
 package BaseTest;
 
 import java.io.FileInputStream;
+
 import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -21,6 +23,7 @@ public class BaseClass {
 	public static Properties config = new Properties();
 	public static Properties loc = new Properties();
 	public static FileInputStream fis;
+	public static Logger log = Logger.getLogger("devpinoyLogger");
 	
 	@BeforeSuite
 	public void setUp() throws IOException {
@@ -28,8 +31,11 @@ public class BaseClass {
 	if(driver==null) {
 		fis= new FileInputStream(System.getProperty("user.dir")+"\\src\\test\\resources\\properties\\Config.properties");
 		config.load(fis);
+		log.info("config file loaded");
+		
 		fis= new FileInputStream(System.getProperty("user.dir")+"\\src\\test\\resources\\properties\\Locators.properties");
 		loc.load(fis);
+		log.debug("Locators file loaded");
 		
 		if (config.getProperty("browser").equals("chrome")){
 			
@@ -43,6 +49,7 @@ public class BaseClass {
 		}
 	
 	driver.get(config.getProperty("url"));
+	log.debug("navigating to website");
 	driver.manage().timeouts().implicitlyWait(Integer.parseInt(config.getProperty("wait")), TimeUnit.SECONDS);
 	
 	}
@@ -59,6 +66,18 @@ public class BaseClass {
 		driver.findElement(By.cssSelector(loc.getProperty(locator))).sendKeys(loc.getProperty(value));
 		
 	}
+	public boolean checkElementPresence(By by) {
+	
+		try {
+			driver.findElement(by);
+			return true;
+		}catch(Throwable t){
+			return false;
+			
+		}
+		
+	}
+		
 	
 
 	@AfterSuite
